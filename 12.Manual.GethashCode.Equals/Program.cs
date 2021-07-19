d50160b7822ab3d779cbbe1aebf994ffa137b881
marcu <marcu@LAPTOP-MTO8J5A0>
2021-07-19 16:44:51 -0300

    Adicionar arquivos de projeto.

---
+++ 12.Manual.GethashCode.Equals/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace _12.Manual.GethashCode.Equals {
+    class Program {
+        static void Main(string[] args) {
+            // ------- > EQUALS
+
+            string a = "Maria";
+            string b = "Alex";
+
+            bool tf = a.Equals(b);
+            Console.WriteLine(tf);
+
+            // ------- > GetHashCode
+
+            string c = "Maria";
+            string d = "Alex";
+            
+            Console.WriteLine(c.GetHashCode());
+            Console.WriteLine(d.GetHashCode());
+
+            // GetHashCode | Bonus | TERNARIA 
+
+            string strResultado;
+            strResultado = c.GetHashCode() == d.GetHashCode() ? "True" : "False";
+            Console.WriteLine(strResultado);
+        }
+    }
+}
